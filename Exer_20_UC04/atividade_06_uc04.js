d0a1784642810a5b82d1f415b4034f5e209799e4
JOSE JACIEL SOUZA CASTRO <jacielcastro100@gmail.com>
2025-08-22 21:18:57 -0300

    Atualização

--- Exer_20_UC04/atividade_06_uc04.js
+++ Exer_20_UC04/atividade_06_uc04.js
@@ -1,9 +1,12 @@
 import PromptSync from "prompt-sync";
 const prompt = PromptSync()
-let tipo = prompt("Qual E o tipo do seu veículo: 1 = carro ")
-let minutos = Number = prompt("Quanto foi o tempo de permanência: ")
-let horas = Math.floor(minutos / 60)   // pega só a parte inteira -> 2
-let minRestantes = minutos % 60        // pega o resto da divisão -> 30
-
-//console.log(`${horas}:${minRestantes}`)
-let carro = 12,caminhao = "caminhão", mot = "moto"
+console.log("-- Informe o tipo do seu carro --")
+console.log("[1] MOTO ")
+console.log("[2] CARRO ")
+console.log("[3] CAMINHÃO")
+let tipo = prompt("Qual o tipo do carro: ")
+let tempo = Number = prompt("Informe o tempo de permanência: ")
+if ((tempo <= 2 && tipo == "1") || (tempo <= 2 && tipo == "2") || (tempo <= 2 && tipo == "3")) {
+    console.log("Tipo do carro ",tipo)
+    console.log("valor da permanência: ")
+} 
--- Exer_20_UC04/atividade_07_uc04.js
+++ Exer_20_UC04/atividade_07_uc04.js
@@ -6,8 +6,10 @@ let resul_01,resul_02,resul_03
 resul_01 = ano % test_01
 resul_02 = ano % test_02
 resul_03 = ano % test_03
-if (resul_01 === 0 && resul_02 === 0 && resul_03 === 0) {
+if ((resul_01 === 0) || (resul_02 !== 0 && resul_03 === 0)) {
     console.log(" Ano Bissexto ")
-} else {
+}else{
     console.log(" Este ano não é bissexto ")
-} 
+}
+   
+ 
